8c56d5be706f3d8218e3cdb4b29417c30b989dd4
wa-pgeorg <philipp.georg@webacumen.de>
2020-03-20 22:47:43 +0100

    dev

--- src/MoorlCmsBrandSlider.php
+++ src/MoorlCmsBrandSlider.php
@@ -2,7 +2,9 @@
 
 namespace MoorlCmsBrandSlider;
 
+use MoorlFoundation\Core\PluginHelpers;
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -15,5 +17,22 @@ class MoorlCmsBrandSlider extends Plugin
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Content/DependencyInjection'));
         $loader->load('manufacturer.xml');
+    }
+
+    public function uninstall(UninstallContext $context): void
+    {
+        parent::uninstall($context);
+
+        if ($context->keepUserData()) {
+            return;
+        }
+
+        PluginHelpers::removeCmsBlocks($this->container, $context->getContext(), [
+            'moorl-brand-slider'
+        ]);
+
+        PluginHelpers::removeCmsSlots($this->container, $context->getContext(), [
+            'moorl-brand-slider'
+        ]);
     }
 }
